b2ff52b07dfc28e6c72edf56501c1335df1d3554
Garbin Huang <garbinh@gmail.com>
2015-07-26 05:12:29 +0000

    add umd module

---
+++ dist/react-toggle.js
@@ -0,0 +1,171 @@
+(function(){
+var r=function(){var e="function"==typeof require&&require,r=function(i,o,u){o||(o=0);var n=r.resolve(i,o),t=r.m[o][n];if(!t&&e){if(t=e(n))return t}else if(t&&t.c&&(o=t.c,n=t.m,t=r.m[o][t.m],!t))throw new Error('failed to require "'+n+'" from '+o);if(!t)throw new Error('failed to require "'+i+'" from '+u);return t.exports||(t.exports={},t.call(t.exports,t,t.exports,r.relative(n,o))),t.exports};return r.resolve=function(e,n){var i=e,t=e+".js",o=e+"/index.js";return r.m[n][t]&&t?t:r.m[n][o]&&o?o:i},r.relative=function(e,t){return function(n){if("."!=n.charAt(0))return r(n,t,e);var o=e.split("/"),f=n.split("/");o.pop();for(var i=0;i<f.length;i++){var u=f[i];".."==u?o.pop():"."!=u&&o.push(u)}return r(o.join("/"),t,e)}},r}();r.m = [];
+r.m[0] = {
+"react": { exports: window.React },
+"react/addons": { exports: window.React },
+"classnames": { exports: window.classNames },
+"x.js": function(module, exports, require){
+"use strict";
+
+var _interopRequire = function (obj) { return obj && obj.__esModule ? obj["default"] : obj; };
+
+var React = _interopRequire(require("react"));
+
+module.exports = React.createClass({
+  displayName: "x.es6",
+
+  render: function render() {
+    return React.createElement(
+      "svg",
+      { width: "10", height: "10", viewBox: "0 0 10 10", xmlns: "http://www.w3.org/2000/svg" },
+      React.createElement(
+        "title",
+        null,
+        "switch-x"
+      ),
+      React.createElement("path", { d: "M9.9 2.12L7.78 0 4.95 2.828 2.12 0 0 2.12l2.83 2.83L0 7.776 2.123 9.9 4.95 7.07 7.78 9.9 9.9 7.776 7.072 4.95 9.9 2.12", fill: "#fff", "fill-rule": "evenodd" })
+    );
+  }
+});
+
+},
+"index.js": function(module, exports, require){
+"use strict";
+
+var _interopRequire = function (obj) { return obj && obj.__esModule ? obj["default"] : obj; };
+
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+var React = _interopRequire(require("react"));
+
+var classNames = _interopRequire(require("classnames"));
+
+var Check = _interopRequire(require("./check"));
+
+var X = _interopRequire(require("./x"));
+
+var addons = require("react/addons").addons;
+
+var PureRenderMixin = addons.PureRenderMixin;
+
+module.exports = React.createClass({
+  mixins: [PureRenderMixin],
+
+  displayName: "Toggle",
+
+  propTypes: {
+    checked: React.PropTypes.bool,
+    defaultChecked: React.PropTypes.bool,
+    onChange: React.PropTypes.func,
+    name: React.PropTypes.string,
+    value: React.PropTypes.string,
+    id: React.PropTypes.string,
+    "aria-labelledby": React.PropTypes.string,
+    "aria-label": React.PropTypes.string
+  },
+
+  getInitialState: function getInitialState() {
+    var checked = false;
+    if ("checked" in this.props) {
+      checked = this.props.checked;
+    } else if ("defaultChecked" in this.props) {
+      checked = this.props.defaultChecked;
+    }
+    return {
+      checked: !!checked,
+      hasFocus: false
+    };
+  },
+
+  componentWillReceiveProps: function componentWillReceiveProps(nextProps) {
+    if ("checked" in nextProps) {
+      this.setState({ checked: !!nextProps.checked });
+    }
+  },
+
+  handleClick: function handleClick(event) {
+    var checkbox = React.findDOMNode(this.refs.input);
+    if (event.target !== checkbox) {
+      event.preventDefault();
+      checkbox.focus();
+      checkbox.click();
+      return;
+    }
+
+    if (!("checked" in this.props)) {
+      this.setState({ checked: checkbox.checked });
+    }
+  },
+
+  handleFocus: function handleFocus() {
+    this.setState({ hasFocus: true });
+  },
+
+  handleBlur: function handleBlur() {
+    this.setState({ hasFocus: false });
+  },
+
+  render: function render() {
+    var classes = classNames("react-toggle", {
+      "react-toggle--checked": this.state.checked,
+      "react-toggle--focus": this.state.hasFocus,
+      "react-toggle--disabled": this.props.disabled
+    });
+
+    return React.createElement(
+      "div",
+      { className: classes, onClick: this.handleClick },
+      React.createElement(
+        "div",
+        { className: "react-toggle-track" },
+        React.createElement(
+          "div",
+          { className: "react-toggle-track-check" },
+          React.createElement(Check, null)
+        ),
+        React.createElement(
+          "div",
+          { className: "react-toggle-track-x" },
+          React.createElement(X, null)
+        )
+      ),
+      React.createElement("div", { className: "react-toggle-thumb" }),
+      React.createElement("input", _extends({
+        ref: "input",
+        onFocus: this.handleFocus,
+        onBlur: this.handleBlur,
+        className: "react-toggle-screenreader-only",
+        type: "checkbox"
+      }, this.props))
+    );
+  }
+});
+
+},
+"check.js": function(module, exports, require){
+"use strict";
+
+var _interopRequire = function (obj) { return obj && obj.__esModule ? obj["default"] : obj; };
+
+var React = _interopRequire(require("react"));
+
+module.exports = React.createClass({
+  displayName: "check.es6",
+
+  render: function render() {
+    return React.createElement(
+      "svg",
+      { width: "14", height: "11", viewBox: "0 0 14 11", xmlns: "http://www.w3.org/2000/svg" },
+      React.createElement(
+        "title",
+        null,
+        "switch-check"
+      ),
+      React.createElement("path", { d: "M11.264 0L5.26 6.004 2.103 2.847 0 4.95l5.26 5.26 8.108-8.107L11.264 0", fill: "#fff", "fill-rule": "evenodd" })
+    );
+  }
+});
+
+}
+};
+ReactToggle = r("index.js");}());
