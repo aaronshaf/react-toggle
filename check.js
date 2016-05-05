f986d495938e3256c66cb6cb29e3101c25891460
Atis <atis@aol.es>
2016-05-05 17:39:07 +0300

    fix fill-rule attribute name

--- check.js
+++ check.js
@@ -16,7 +16,7 @@ module.exports = React.createClass({
         null,
         "switch-check"
       ),
-      React.createElement("path", { d: "M11.264 0L5.26 6.004 2.103 2.847 0 4.95l5.26 5.26 8.108-8.107L11.264 0", fill: "#fff", "fill-rule": "evenodd" })
+      React.createElement("path", { d: "M11.264 0L5.26 6.004 2.103 2.847 0 4.95l5.26 5.26 8.108-8.107L11.264 0", fill: "#fff", fillRule: "evenodd" })
     );
   }
 });
--- x.es6.js
+++ x.es6.js
@@ -7,7 +7,7 @@ export default React.createClass({
         <title>
           switch-x
         </title>
-        <path d="M9.9 2.12L7.78 0 4.95 2.828 2.12 0 0 2.12l2.83 2.83L0 7.776 2.123 9.9 4.95 7.07 7.78 9.9 9.9 7.776 7.072 4.95 9.9 2.12" fill="#fff" fill-rule="evenodd"/>
+        <path d="M9.9 2.12L7.78 0 4.95 2.828 2.12 0 0 2.12l2.83 2.83L0 7.776 2.123 9.9 4.95 7.07 7.78 9.9 9.9 7.776 7.072 4.95 9.9 2.12" fill="#fff" fillRule="evenodd"/>
       </svg>
     )
   }
