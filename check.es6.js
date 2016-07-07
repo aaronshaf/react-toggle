4a08ad9e42cf19088351b708f77d10ed8122aa71
Jeff Bellsey <jeff.bellsey@lookout.com>
2016-07-07 13:00:01 -0700

    Fix SVG errors
    * Removed "xmlns" attributes, which are optional anyway
    * Renamed "fill-rule" attributes to "fillRule" for React compatibility

--- check.es6.js
+++ check.es6.js
@@ -3,11 +3,11 @@ import React from 'react'
 export default React.createClass({
   render() {
     return (
-      <svg width="14" height="11" viewBox="0 0 14 11" xmlns="http://www.w3.org/2000/svg">
+      <svg width="14" height="11" viewBox="0 0 14 11">
         <title>
           switch-check
         </title>
-        <path d="M11.264 0L5.26 6.004 2.103 2.847 0 4.95l5.26 5.26 8.108-8.107L11.264 0" fill="#fff" fill-rule="evenodd"/>
+        <path d="M11.264 0L5.26 6.004 2.103 2.847 0 4.95l5.26 5.26 8.108-8.107L11.264 0" fill="#fff" fillRule="evenodd"/>
       </svg>
     )
   }
