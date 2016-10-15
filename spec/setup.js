7232e272bebc15e853fa8944c2d58e1f5863e0e9
Anton Strömkvist <hi@ahst.ro>
2016-10-15 19:24:53 +0200

    Setup testing with Mocha, Chai, and Enzyme

---
+++ spec/setup.js
@@ -0,0 +1,16 @@
+import jsdom from 'jsdom'
+
+const exposedProperties = ['window', 'navigator', 'document']
+
+global.document = jsdom.jsdom('')
+global.window = document.defaultView
+Object.keys(document.defaultView).forEach((property) => {
+  if (typeof global[property] === 'undefined') {
+    exposedProperties.push(property)
+    global[property] = document.defaultView[property]
+  }
+})
+
+global.navigator = {
+  userAgent: 'node.js'
+}
