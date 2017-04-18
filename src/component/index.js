c8f6eff4a37f261ef2fc14857cc543b88a581425
Ben Ripkens <bripkens@gmail.com>
2017-04-18 11:41:28 +0200

    Don't use deprecated React.PropTypes API

--- src/component/index.js
+++ src/component/index.js
@@ -1,5 +1,6 @@
-import React, { PureComponent, PropTypes } from 'react'
+import React, { PureComponent } from 'react'
 import classNames from 'classnames'
+import PropTypes from 'prop-types'
 import Check from './check'
 import X from './x'
 import { pointerCoord } from './util'
