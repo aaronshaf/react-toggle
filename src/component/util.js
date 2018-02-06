e8c7aea8989c1a6d03109517e2882ffae20baa03
Stephen King <stephen.king@cen.centeva.com>
2018-02-06 08:23:05 -0700

    Create checkForLabel, hasParentLabel, hasIdThatMatchesLabelFor, hasAriaLabelOrAraiLabelledbyAttr utils

--- src/component/util.js
+++ src/component/util.js
@@ -18,3 +18,26 @@ export function pointerCoord (event) {
   }
   return { x: 0, y: 0 }
 }
+
+export function checkForLabel (toggleContainer, input) {
+  return (hasParentLabel(toggleContainer, 'LABEL') || hasIdThatMatchesLabelFor(input) || hasAriaLabelOrAriaLabelledbyAttr(input)) ||
+  console.warn('There seems to not be any associated label for the react-toggle element.  https://www.w3.org/standards/webdesign/accessibility')
+}
+
+export function hasParentLabel (element, tagName) {
+  return (element.parentNode && element.parentNode.nodeName === tagName)
+    ? true
+    : (!element.parentNode)
+      ? false
+      : hasParentLabel(element.parentNode, tagName)
+}
+
+export function hasIdThatMatchesLabelFor (element, document = window.document) {
+  return element.id
+    ? Array.from(document.querySelectorAll('label')).some(label => label.htmlFor === element.id)
+    : false
+}
+
+export function hasAriaLabelOrAriaLabelledbyAttr (element) {
+  return !!element.getAttribute('aria-label') || !!element.getAttribute('aria-labelledby')
+}
